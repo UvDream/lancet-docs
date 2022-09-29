28cee17deedbeda9ced52da649eb74106b52df9b
ShutDown <uvdream@163.com>
2022-09-29 14:33:19 +0800

    feat: algolia配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -29,6 +29,12 @@ export = defineConfig((ctx) => ({
     },
     themeConfig: {
         logo: "/logo.png",
+        // algolia: {
+        //     apiKey: '<API_KEY>',
+        //     indexName: '<INDEX_NAME>'
+        //     // 如果 Algolia 没有为你提供 `appId` ，使用 `BH4D9OD16A` 或者移除该配置项
+        //     appId: '<APP_ID>',
+        // },
         locales: {
             "/": {
                 selectText: "选择语言",
